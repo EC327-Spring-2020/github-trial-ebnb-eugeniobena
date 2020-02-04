64111ee1aaa4850bbc0ffacfe781725ee26a6c92
Benavides Benitez <ebena@signals30.bu.edu>
2020-02-04 17:12:32 -0500

    first commit

---
+++ Desktop/Lab2/lab2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout<<"Eugenio and Pizza"<<endl;
+	return 0;
+}
---
+++ Desktop/Lab2/mathTrick.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+	int num =0;
+	cout<<"Enter a number:"<<endl;
+	cin>>num;
+	 result = (num*4+6)/(3+(num%2)) - num;
+	cout<<"The result is "<<result<<endl;
+	return 0;
+}
